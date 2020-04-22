0187c2b9d4ec7bb98a8959356abb3b194c107791
snag <somjitnag192@gmail.com>
2020-04-22 06:00:32 +0530

    DTO mapping not working

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,18 +1,65 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { TaskStatus, Task } from './task.model';
+import { FilterTaskDto } from './dto/filter-task-dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks() {
-    return this.taskService.getAllTasks();
+  getTasks(@Query() filterDto: FilterTaskDto) {
+    if (Object.keys(filterDto).length) {
+      console.log(`getTasks:: ${JSON.stringify(filterDto)}`);
+      return this.taskService.getFilteredTasks(filterDto);
+    } else {
+      return this.taskService.getAllTasks();
+    }
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: string) {
+    console.log(`passed value of id: ${id}`);
+    return this.taskService.getTaskById(id);
   }
 
   @Post()
   createTask(@Body() dto: CreateTaskDto) {
     return this.taskService.createTask(dto);
+  }
+
+  @Delete('delete/:id')
+  deleteTask(@Param('id') id: string): Task[] {
+    return this.taskService.deleteTask(id);
+  }
+
+  /**
+   * Patch best practices:
+   *
+   * url format: PATCH http://ip:port/<resource>/<id of item to patch>/<what part of item to patch> . for us:
+   * resource      : tasks
+   * id            : provided
+   * part to patch : status
+   *
+   * Also, provide the new patched value in the body
+   *
+   * @param id
+   */
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id') id: string,
+    @Body('status') newStatus: TaskStatus,
+  ): Task {
+    return this.taskService.updateTaskStatus(id, newStatus);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Get } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid/v1';
+import { v1 as uuidv1 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { FilterTaskDto } from './dto/filter-task-dto';
 
 @Injectable()
 export class TasksService {
@@ -11,17 +12,52 @@ export class TasksService {
     return this.tasks;
   }
 
+  getFilteredTasks(filter: FilterTaskDto): Task[] {
+    const { status, search } = filter;
+
+    let filteredTasks = this.tasks;
+
+    if (status) {
+      filteredTasks = filteredTasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      filteredTasks = filteredTasks.filter(task => {
+        task.title.includes(search) || task.description.includes(search);
+      });
+    }
+
+    return filteredTasks;
+  }
+
+  getTaskById(id: string) {
+    return this.tasks.find(task => task.id === id);
+  }
+
   createTask(dto: CreateTaskDto): Task {
     const { title, description } = dto;
 
     const task: Task = {
-      id: uuid(),
+      id: uuidv1(),
       title,
       description,
       status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string) {
+    this.tasks.forEach((task, index) => {
+      if (task.id === id) this.tasks.splice(index, 1);
+    });
+    return this.tasks;
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    const task = this.getTaskById(taskId);
+    task.status = newStatus;
     return task;
   }
 }

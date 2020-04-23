3b06a0148c9b332254874309ab362a17a6647093
snag <somjitnag192@gmail.com>
2020-04-24 04:30:39 +0530

    Persistence Sorted! at least in my mac's debug mode, yarn start:dev continues to give weird results

--- src/tasks/tasks.controller.v2.ts
+++ src/tasks/tasks.controller.v2.ts
@@ -1,29 +1,57 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
   Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Body,
   Delete,
   Patch,
   Query,
-  UsePipes,
-  ValidationPipe,
-  ParseIntPipe,
 } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task-dto';
-import { TaskStatus } from './task.model';
-import { FilterTaskDto } from './dto/filter-task-dto';
-import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+// import { CreateTaskDto } from './dto/create-task-dto';
+// import { TaskStatus } from './task.model';
+// import { FilterTaskDto } from './dto/filter-task-dto';
+// import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { TaskPersistenceService } from './tasks.persistent.service';
 import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task.model';
+import { FilterTaskDto } from './dto/filter-task-dto';
 
 @Controller('api/v2/tasks')
 export class TasksControllerV2 {
   constructor(private taskService: TaskPersistenceService) {}
 
+  @Get()
+  getTasks(@Query(ValidationPipe) dto: FilterTaskDto): Promise<Task[]> {
+    return this.taskService.getTasks(dto);
+  }
+
   @Get(':id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.taskService.getTaskById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() dto: CreateTaskDto): Promise<Task> {
+    return this.taskService.createTask(dto);
+  }
+
+  @Delete(':id')
+  deleteTaskById(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.taskService.deleteTask(id);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.taskService.updateStatus(id, status);
   }
 }
--- src/tasks/tasks.persistent.service.ts
+++ src/tasks/tasks.persistent.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, Get, NotFoundException, Inject } from '@nestjs/common';
-import { TaskI, TaskStatus } from './task.model';
-import { v1 as uuidv1 } from 'uuid';
+import { TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { FilterTaskDto } from './dto/filter-task-dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TaskPersistenceService {
@@ -12,13 +12,31 @@ export class TaskPersistenceService {
     @InjectRepository(TaskRepository) private repository: TaskRepository,
   ) {}
 
+  async getTasks(filterTaskDto: FilterTaskDto): Promise<Task[]> {
+    return this.repository.getTasks(filterTaskDto);
+  }
+
   async getTaskById(id: number) {
     const found = await this.repository.findOne(id);
-
     if (!found) {
       throw new NotFoundException(`Did not find a task with id: ${id}`);
     }
-
     return found;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.repository.createTask(createTaskDto);
+  }
+
+  async deleteTask(id: number): Promise<void> {
+    const deletedTask = await this.repository.delete(id);
+    console.log(`Deleted Task: ${JSON.stringify(deletedTask)}`);
+  }
+
+  async updateStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await task.save();
+    return task;
   }
 }
